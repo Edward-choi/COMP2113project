2bc39be45436bbe3c7a1e1bde31399e56a20b9fc
Edward-choi <72147513+Edward-choi@users.noreply.github.com>
2020-11-16 16:18:15 +0800

    Create MC2.cpp

---
+++ MC2.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+    srand (time(NULL));
+    char s1;
+    char s2;
+    char s3;
+    int numWrong = 0;
+    while(true){
+        cout << "Oops, you have triggered a special event! Now you are going to answer three multiple choice questions. Answer at least two correctly to get the secret reward, or you will receive a random penalty!" << endl;
+        cout << "Question 1: Which one is the smallest conntry in the world?\nA Tuvalu    B Principality of Liechtenstein    C Vatican City State    D Republic of San Marino" << endl;
+        cout << "Your Choice: ";
+        cin >> s1;
+        if (s1=='C'||s1=='c') cout << "You are correct! Well done!" << endl;
+        else{
+            cout << "Wrong Answer! The correct answer is A." << endl;
+            numWrong++;
+        } 
+        cout << "Question 2: Who is the all-time leading scorer in NBA history?\nA Kareem-Abdul-Jabbar    B Kobe Bryant    C Karl Malone    D Lebron James" << endl;
+        cout << "Your Choice: ";
+        cin >> s2;
+        if (s2=='A'||s2=='a') cout << "You are correct! Well done!" << endl;
+        else{
+            cout << "Wrong Answer! The correct answer is D." << endl;
+            numWrong++;
+        } 
+        if (numWrong==2) {
+            cout << "You have got two answers wrong. Better luck next time!" << endl;
+            break;
+        }
+        cout << "Question 3: Which scientific law states that for every action in nature there is an equal and opposite reaction.?\nA Law of universal gravitation    B Newton's Second Law    C Coulomb’s Law    D Newton's Third Law" << endl;
+        cout << "Your Choice: ";
+        cin >> s3;
+        if (s3=='D'||s3=='d') cout << "You are correct! Well done!" << endl;
+        else{
+            cout << "Wrong Answer! The correct answer is B." << endl;
+            numWrong++;
+        }
+        if (numWrong==2) {
+            cout << "You have got two answers wrong. Better luck next time!" << endl;
+            break;
+        }
+        cout << "Congratulations! You pass the challenge!" << endl;
+        break;
+    }
+}
