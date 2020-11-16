a5dd9e95bd652020a03ca8c623f3e0e69a345114
Edward-choi <72147513+Edward-choi@users.noreply.github.com>
2020-11-16 15:57:52 +0800

    Update MC.cpp

--- MC.cpp
+++ MC.cpp
@@ -13,18 +13,18 @@ int main()
     int numWrong = 0;
     while(true){
         cout << "Oops, you have triggered a special event! Now you are going to answer three multiple choice questions. Answer at least two correctly to get the secret reward, or you will receive a random penalty!" << endl;
-        cout << "Question 1: Which US president was not featured in the Mount Rushmore?\nA Franklin Delano Roosevelt    B Thomas Jefferson    C Theodore Roosevelt    D Abraham Lincoln" << endl;
+        cout << "Question 1: Which US president was not featured in the Mount Rushmore?\nA Franklin D. Roosevelt    B Thomas Jefferson    C Theodore Roosevelt    D Abraham Lincoln" << endl;
         cout << "Your Choice: ";
         cin >> s1;
-        if (s1=='A') cout << "You are correct! Well done!" << endl;
+        if (s1=='A'||s1=='a') cout << "You are correct! Well done!" << endl;
         else{
             cout << "Wrong Answer! The correct answer is A." << endl;
             numWrong++;
         } 
-        cout << "Question 2: When was The Univerisity of Hong Kong found?\nA 1874    B 1853    C 1881    D 1911" << endl;
+        cout << "Question 2: When was The Univerisity of Hong Kong founded?\nA 1874    B 1853    C 1881    D 1911" << endl;
         cout << "Your Choice: ";
         cin >> s2;
-        if (s2=='D') cout << "You are correct! Well done!" << endl;
+        if (s2=='D'||s2=='d') cout << "You are correct! Well done!" << endl;
         else{
             cout << "Wrong Answer! The correct answer is D." << endl;
             numWrong++;
@@ -36,7 +36,7 @@ int main()
         cout << "Question 3: Who is the first person to win two Nobel prizes in different fields?\nA Linus Carl Pauling    B Marie Skłodowska Curie    C John Bardeen    D Frederick Sanger" << endl;
         cout << "Your Choice: ";
         cin >> s3;
-        if (s3=='B') cout << "You are correct! Well done!" << endl;
+        if (s3=='B'||s3=='b') cout << "You are correct! Well done!" << endl;
         else{
             cout << "Wrong Answer! The correct answer is B." << endl;
             numWrong++;
@@ -46,5 +46,7 @@ int main()
             break;
         }
         cout << "Congratulations! You pass the challenge!" << endl;
+        break;
     }
 }
+
