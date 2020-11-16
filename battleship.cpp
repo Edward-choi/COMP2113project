9d08981378877beb464ce5633439cc7e69f19833
KwokKaTin <73277881+KwokKaTin@users.noreply.github.com>
2020-11-16 16:57:12 +0800

    Update battleship.cpp

--- battleship.cpp
+++ battleship.cpp
@@ -9,17 +9,32 @@ void showBoard(string board[11][11]) {
         }
         cout << '\n';
     }
+    cout << '\n';
 }
 
 void place_Boats(string board[11][11], string x, int k) {
     cout << "From which square to which square? ";
     string start;
     string end;
+    string empty = "O";
+    int count = 0;
     cin >> start >> end;
     int x1 = int(start[0]) - 65;
     int y1 = int(start[1]) - 48;
     int x2 = int(end[0]) - 65;
     int y2 = int(end[1]) - 48;
+    if (start.length() != 2 || end.length() != 2) {
+        cout << "Incorrect index format!" << '\n';
+        place_Boats(board, x, k);
+    }
+    if ((x1 < 0 || x1 > 9) && (x2 < 0 || x1 > 9)) {
+        cout << "Row index must be between 'A' and 'J'!" << '\n';
+        place_Boats(board, x, k);
+    }
+    if ((y1 < 0 || y1 > 9) && (y2 < 0 || y1 > 9)) {
+        cout << "Column index must be between 0 and 9!" << '\n';
+        place_Boats(board, x, k);
+    }
     if (x1 != x2 && y1 != y2) {
         cout << "Boat must be placed horizontally or vertically!" << '\n';
         place_Boats(board, x, k);
@@ -29,9 +44,22 @@ void place_Boats(string board[11][11], string x, int k) {
     } else {
         for (int i = x1; i < x2+1; i++) {
             for (int j = y1; j < y2+1; j++) {
-                board[i+1][j+1] = x;
+                if (empty.compare(board[i+1][j+1]) == 0) {
+                    count++;
+                }
             }
         }
+        if (count == k) {
+            for (int i = x1; i < x2+1; i++) {
+                for (int j = y1; j < y2+1; j++) {
+                    board[i+1][j+1] = x;
+                }
+            }
+        } else {
+            cout << "Ships cannot overlap each other!" << '\n';
+            place_Boats(board, x, k);
+        }
+
     }
     showBoard(board);
 }
