c2b93f1010ce161467dff7cfbf6983f341b90dad
Edward-choi <72147513+Edward-choi@users.noreply.github.com>
2020-11-21 03:50:21 +0800

    Create funnyFeatures.cpp

---
+++ funnyFeatures.cpp
@@ -0,0 +1,32 @@
+string name;
+int i;
+cout << "Lily: Welcome to the World of Games, Brave! I am elf Lily, your guide here! Could you please tell me your name?"; << endl;
+cout << "You: ";
+cin << name;
+cout << "Lily: " << name << "? what a pretty name! Unfortunately, I am very sorry to tell you that our Elf Kingdom is facing an unprecedented crisis: our hateful neighbour, the Orc Kingdom is launching an attack on us. They are too strong to be defeated! "<< name << ", you are our last hope. Please help us to beat them! "<< endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "You: Lily, there is no need to worry. I will help you." << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "Lily: Thank you very much, " << name << "! Now you are going to play a battle ship game with the king of orc! Defeat him to rescue our kingdom?" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "You: What? You mean I can stop the war with a game? I can't believe it!" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "Lily: Of course! Here is the World of Games. Everything is decided by games. No Game No Life!" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "You: Well, okay..." << endl;
+
+Win
+cout << "Lily: Thank you, Brave! You save our kingdom! We will remember your blessing forever!" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "You: You are welcome, Lily. It is time for me to leave. Goodbye, and thank you. << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "Lily: Goodbye, my hero" << endl;
+
+Lose
