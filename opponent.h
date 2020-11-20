5d54c207f231fa538e738bc6cbd06cf593f4f31c
KwokKaTin <73277881+KwokKaTin@users.noreply.github.com>
2020-11-20 15:11:46 +0800

    Update opponent.h

--- opponent.h
+++ opponent.h
@@ -1,5 +1,5 @@
 // opponent.h 
 #ifndef OPPONENT_H 
 #define OPPONENT_H
-void Opponent(int &low, int &high, int &guess, int target)
+void Opponent(int &low, int &high, int &guess, int target);
 #endif
