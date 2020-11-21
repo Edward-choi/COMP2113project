47a819e347fa4aa1d4dec3c40ec988e2638fec1f
Edward-choi <72147513+Edward-choi@users.noreply.github.com>
2020-11-21 18:54:24 +0800

    Update MC4.cpp

--- MC4.cpp
+++ MC4.cpp
@@ -5,6 +5,7 @@
 #include "mc4.h"
 using namespace std;
 
+//this method asks the player 3 questions and store the answers. If the player answers at least two questions correctly, it returns 0. Else, it returns 1.
 int MC4()
 {
     srand (time(NULL));
