8974d879fb1e2739407779c2fa586ae6bb92c286
KwokKaTin <73277881+KwokKaTin@users.noreply.github.com>
2020-11-21 20:17:18 +0800

    Update MC4.cpp

--- MC4.cpp
+++ MC4.cpp
@@ -15,7 +15,7 @@ int MC4()
     int numWrong = 0;
     while(true){
         cout << "Oops, you have triggered a special event! Now you are going to answer three multiple choice questions. Answer at least two correctly to get the secret reward, or you will receive a random penalty!" << endl;
-        cout << "Question 1: Which is the capital city of Australia?\nA  Canberra    B Sydney    C Melbourne    D Auckland" << endl;
+        cout << "Question 1: What is 2+2?\nA 4     B 69,420    C 177,013    D 114,514" << endl;
         cout << "Your Choice: ";
         cin >> s1;
         if (s1=='A'||s1=='a') cout << "You are correct! Well done!" << endl;
@@ -23,7 +23,7 @@ int MC4()
             cout << "Wrong Answer! The correct answer is A." << endl;
             numWrong++;
         } 
-        cout << "Question 2: Which is the fifth largest country in the world?\nA Australia    B India    C China    D Argentina    E None of the above" << endl;
+        cout << "Question 2: How do you spell UFO?\nA OFU    B ABC    C Ewe Eff Ohh    D 42    E None of the above" << endl;
         cout << "Your Choice: ";
         cin >> s2;
         if (s2=='E'||s2=='e') cout << "You are correct! Well done!" << endl;
@@ -35,12 +35,12 @@ int MC4()
             cout << "You have got two answers wrong. Better luck next time!" << endl;
             break;
         }
-        cout << "Question 3: Which manga was not published by Weekly Shōnen Jump?\nA Slam Dunk    B Attack on Titan    C Naruto    D To Love Ru    E None of the above" << endl;
+        cout << "Question 3: Are the professors of COMP2113_ENGG1340 awesome?\nA Absolutely   B For sure    C I'm positive    D Hell yeah    E No" << endl;
         cout << "Your Choice: ";
         cin >> s3;
-        if (s3=='B'||s3=='b') cout << "You are correct! Well done!" << endl;
+        if (s3 !='E'||s3 !='e') cout << "You are correct! Well done!" << endl;
         else{
-            cout << "Wrong Answer! The correct answer is B." << endl;
+            cout << "Wrong Answer! Come on" << endl;
             numWrong++;
         }
         if (numWrong==2) {
