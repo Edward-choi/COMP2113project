a4c3a03066b52fcc5b6542d7a500f92a9b337c6c
Edward-choi <72147513+Edward-choi@users.noreply.github.com>
2020-11-21 11:29:29 +0800

    Update funnyFeatures.cpp

--- funnyFeatures.cpp
+++ funnyFeatures.cpp
@@ -1,32 +1,39 @@
 string name;
 int i;
-cout << "Lily: Welcome to the World of Games, Brave! I am elf Lily, your guide here! Could you please tell me your name?"; << endl;
+cout << "Kong Ming: Welcome to the Three Kingdoms, Brave! I am elf Kong Ming (孔明) , your guide here! Could you please tell me your name?"; << endl;
 cout << "You: ";
 cin << name;
-cout << "Lily: " << name << "? what a pretty name! Unfortunately, I am very sorry to tell you that our Elf Kingdom is facing an unprecedented crisis: our hateful neighbour, the Orc Kingdom is launching an attack on us. They are too strong to be defeated! "<< name << ", you are our last hope. Please help us to beat them! "<< endl;
+cout << "Kong Ming: " << name << "? what a pretty name! Unfortunately, I am very sorry to tell you that our Shu Kingdom (蜀國) is facing an unprecedented crisis: our hateful neighbour, the Wei Kingdom（魏國）is launching an attack on us. They are too strong to be defeated! "<< name << ", you are our last hope. Please help us to beat them! "<< endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "You: Lily, there is no need to worry. I will help you." << endl;
+cout << "You: Kong Ming, there is no need to worry. I will help you." << endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "Lily: Thank you very much, " << name << "! Now you are going to play a battle ship game with the king of orc! Defeat him to rescue our kingdom?" << endl;
+cout << "Kong Ming: Thank you very much, " << name << "! Now you are going to lead our soldiers to fight with the Wei army led by Cao Cao（曹操）, the king of Wei! Defeat him to rescue our kingdom?" << endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "You: What? You mean I can stop the war with a game? I can't believe it!" << endl;
+cout << "You: I see... but what should I do to defeat him?" << endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "Lily: Of course! Here is the World of Games. Everything is decided by games. No Game No Life!" << endl;
+cout << "Kong Ming: The battle will take place in the Red Cliffs（赤壁）. Please sink all Cao's balttleships!" << endl;
 cout << "Press 0 to continue";
 cin >> i;
 cout << "You: Well, okay..." << endl;
 
 Win
-cout << "Lily: Thank you, Brave! You save our kingdom! We will remember your blessing forever!" << endl;
+cout << "Kong Ming: Thank you, Brave! You save our kingdom! We will remember your blessing forever!" << endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "You: You are welcome, Lily. It is time for me to leave. Goodbye, and thank you. << endl;
+cout << "You: You are welcome, Kong Ming. It is time for me to leave. Goodbye, and thank you. << endl;
 cout << "Press 0 to continue";
 cin >> i;
-cout << "Lily: Goodbye, my hero" << endl;
+cout << "Kong Ming: Goodbye, hero." << endl;
 
 Lose
+cout << "Despite your stubborn resistance， Cao Cao led the Wei army to win the battle" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "Cao Cao: What a costly victory! Brave, to me, you are the greatest threat, and I am not going to let you go. Send this him to the prison!" << endl;
+cout << "Press 0 to continue";
+cin >> i;
+cout << "You: NO!!!" << endl;
